4481712b5a0f2e30829d146b5a675cdb984bc27e
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-10-07 00:04:35 +0300

    Initial Commit

---
+++ ToDoSwiftData/ToDoSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftDataApp.swift
+//  ToDoSwiftData
+//
+//  Created by Beyza Nur Tekerek on 7.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
